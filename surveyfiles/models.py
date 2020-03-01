3a03ce1303f419d10557ebe51560bda3ecfe8986
jxue <jxue@globalraymac.ca>
2020-03-01 13:03:56 -0700

    add exception log in get time from log file lines

--- surveyfiles/models.py
+++ surveyfiles/models.py
@@ -302,14 +302,18 @@ class SurveyFileAutomation(models.Model):
     def latest_log_time(self):
         if self.log_path is not None and os.path.isfile(self.log_path):
             line_time_str_pattern = re.compile('^\d{4}-(\d{2}-\d{2}\s\d{2}:\d{2}):\d{2}\s')
-            with open(self.log_path) as reader:
-                lines = reader.readlines()
-                lines_with_time = [line for line in lines if re.match(line_time_str_pattern, line)]
-                if len(lines_with_time) == 0:
-                    return None
-                last_line = lines_with_time[-1]
-                last_time = re.search(line_time_str_pattern, last_line).groups()[0]
-                return datetime.datetime.strptime(last_time, '%m-%d %H:%M').strftime('%H:%M on %b %d')
+            try:
+                with open(self.log_path) as reader:
+                    lines = reader.readlines()
+                    lines_with_time = [line for line in lines if re.match(line_time_str_pattern, line)]
+                    if len(lines_with_time) == 0:
+                        return None
+                    last_line = lines_with_time[-1]
+                    last_time = re.search(line_time_str_pattern, last_line).groups()[0]
+                    return datetime.datetime.strptime(last_time, '%m-%d %H:%M').strftime('%H:%M on %b %d')
+            except Exception as e:
+                logger_model.exception(e)
+                return None
         else:
             return None
 
