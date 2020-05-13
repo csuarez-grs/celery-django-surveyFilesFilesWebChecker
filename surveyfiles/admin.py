c9d38ebde70ab2d60ba09566f8bafcd5d65676d5
jxue <jxue@globalraymac.ca>
2020-05-13 10:14:50 -0600

    update admin with list display

--- surveyfiles/admin.py
+++ surveyfiles/admin.py
@@ -5,4 +5,10 @@ from django.contrib import admin
 
 from .models import SurveyFileAutomation
 
-admin.site.register(SurveyFileAutomation)
+
+class SurveyFileAdmin(admin.ModelAdmin):
+    list_display = ('job_no', 'site_no', 'document', 'uploaded_time', 'uploader_name', 'qc_passed',
+                    'exporting_types_selected', 'automation_status', 'errors')
+
+
+admin.site.register(SurveyFileAutomation, SurveyFileAdmin)
