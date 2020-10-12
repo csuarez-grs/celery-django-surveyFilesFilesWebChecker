b13c04e9ea277a83f497b6d5b98c79dce9f91709
jxue <jxue@globalraymac.ca>
2020-10-11 22:03:32 -0600

    Added a few fields into api.

--- surveyfiles/api/serializers.py
+++ surveyfiles/api/serializers.py
@@ -9,4 +9,5 @@ class SurveyFileAutomationSerializer(serializers.HyperlinkedModelSerializer):
         fields = (
             'url', 'tracking_id', 'document_name', 'job_no', 'site_no', 'uploader_name', 'surveyor_name',
             'total_qc_time', 'qc_passed', 'total_automation_time', 'automation_status', 'total_profiles',
-            'total_points', 'total_structures', 'total_profiles_meters')
+            'total_points', 'total_structures', 'total_profiles_meters', 'create_gis_data',
+            'create_client_report', 'exporting_types_selected', 'transmittals_folder')
